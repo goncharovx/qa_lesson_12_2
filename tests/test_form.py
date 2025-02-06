0d8713703fe3ebbc88eaa89c0aa4219c0aa102a2
Goncharov Anton <a.goncharrov@icloud.com>
2025-02-07 01:24:41 +0300

    upd test actions

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,25 +1,24 @@
-import os
 import allure
-from selene import have, be
-from selene.api import s
+import time
+import os
+from selene import be, have, browser, command
+from selene.support.shared.jquery_style import s
 from utils import attach
 
-current_dir = os.path.dirname(__file__)
-file_path = os.path.join(current_dir, 'resources', 'pic.png')
 
 def test_registration_form(open_browser):
-    browser = open_browser
-
     first_name = "Sonic"
     last_name = "Syndicate"
 
     with allure.step('Открыть форму регистрации'):
         browser.open('https://demoqa.com/automation-practice-form')
-        browser.execute_script("$('footer').remove()")
-        browser.execute_script("$('#fixedban').remove()")
-        browser.execute_script("document.querySelectorAll('iframe').forEach(iframe => iframe.remove())")
+
+        browser.execute_script("document.querySelectorAll('iframe').forEach(e => e.remove());")
+        browser.execute_script("document.querySelectorAll('.ads, .ad-container, #fixedban, footer').forEach(e => e.remove());")
+        # time.sleep(1)
+        browser.execute_script("document.querySelectorAll('iframe[id^=\"google_ads_iframe\"]').forEach(e => e.remove());")
+
         attach.add_screenshot(browser)
-        attach.add_logs(browser)
         attach.add_html(browser)
 
     with allure.step('Заполнить ФИО'):
@@ -32,7 +31,7 @@ def test_registration_form(open_browser):
         attach.add_screenshot(browser)
 
     with allure.step('Выбрать пол'):
-        s('[for="gender-radio-1"]').click()
+        s('[for="gender-radio-1"]').perform(command.js.scroll_into_view).should(be.clickable).click()
         attach.add_screenshot(browser)
 
     with allure.step('Заполнить поле ввода телефон'):
@@ -40,7 +39,9 @@ def test_registration_form(open_browser):
         attach.add_screenshot(browser)
 
     with allure.step('Установить дату рождения'):
-        s('#dateOfBirthInput').should(be.clickable).click()
+        birth_input = s('#dateOfBirthInput')
+        birth_input.perform(command.js.scroll_into_view).should(be.clickable).click()
+
         s('.react-datepicker__month-select').click().s('[value="2"]').click()
         s('.react-datepicker__year-select').click().s('[value="1960"]').click()
         s('.react-datepicker__day--003:not(.react-datepicker__day--outside-month)').click()
@@ -51,11 +52,12 @@ def test_registration_form(open_browser):
         attach.add_screenshot(browser)
 
     with allure.step('Выбрать хобби'):
-        s('[for="hobbies-checkbox-1"]').click()
-        s('[for="hobbies-checkbox-3"]').click()
+        s('[for="hobbies-checkbox-1"]').perform(command.js.scroll_into_view).should(be.clickable).click()
+        s('[for="hobbies-checkbox-3"]').should(be.clickable).click()
         attach.add_screenshot(browser)
 
     with allure.step('Загрузить файл'):
+        file_path = os.path.abspath("tests/resources/pic.png")
         s('#uploadPicture').send_keys(file_path)
         attach.add_screenshot(browser)
 
@@ -64,11 +66,27 @@ def test_registration_form(open_browser):
         attach.add_screenshot(browser)
 
     with allure.step('Выбрать штат'):
-        s('#state').click().s('#react-select-3-option-0').click()
+        state_element = s('#state')
+        state_element.perform(command.js.scroll_into_view).should(be.visible)
+
+        try:
+            state_element.click()
+        except:
+            browser.execute_script("arguments[0].click();", state_element.get_actual_webelement())
+
+        s('#react-select-3-option-0').should(be.visible).click()
         attach.add_screenshot(browser)
 
     with allure.step('Выбрать город'):
-        s('#city').click().s('#react-select-4-option-0').click()
+        city_element = s('#city')
+        city_element.should(be.visible)
+
+        try:
+            city_element.click()
+        except:
+            browser.execute_script("arguments[0].click();", city_element.get_actual_webelement())
+
+        s('#react-select-4-option-0').should(be.visible).click()
         attach.add_screenshot(browser)
 
     with allure.step('Подтвердить регистрацию'):
@@ -85,11 +103,3 @@ def test_registration_form(open_browser):
         s('.table-responsive').should(have.text('03 March,1960'))
         s('.table-responsive').should(have.text('Maths'))
         s('.table-responsive').should(have.text('Sports'))
-        s('.table-responsive').should(have.text('Music'))
-        s('.table-responsive').should(have.text('pic.png'))
-        s('.table-responsive').should(have.text('Moscow 5'))
-        s('.table-responsive').should(have.text('NCR Delhi'))
-        attach.add_screenshot(browser)
-        attach.add_logs(browser)
-        attach.add_html(browser)
-        attach.add_video(browser)
